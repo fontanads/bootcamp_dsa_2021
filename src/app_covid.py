f75e7c6b55dd3a3cf52d3fa556ae6411f00e8542
Bruno Fontana <fontanads@gmail.com>
2021-01-13 18:16:08 -0300

    nova versão

--- src/app_covid.py
+++ src/app_covid.py
@@ -1,27 +1,33 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-
 from helpers import preprocessamento, download_url, load_dataframe, casos_por_obitos_do_estado, plot_timeseries_casos_por_obitos
 
-
-def main():
-    st.title('Análise de Registros de Caso e Óbitos de COVID-19 no Brasil')
-
+@st.cache
+def carrega_dataframe(output_path):
     df = load_dataframe(output_path)
     df = preprocessamento(df)
-
     todos_os_estados   = sorted(df['estado'].dropna().unique().tolist())
     todos_os_estados.insert(0,'Brasil')
-    estado_selecionado = st.sidebar.selectbox('Selecione o Estado', todos_os_estados, index=0,)
+    return df, todos_os_estados
 
-    df_estado = casos_por_obitos_do_estado(df, estado=estado_selecionado, casos_por_obitos=1)
+def carrega_dados_e_plota(df, estado_selecionado, casos_por_obitos='Casos por óbito'):
+    df_estado = casos_por_obitos_do_estado(df, estado=estado_selecionado, casos_por_obitos=casos_por_obitos)
+    figura = plot_timeseries_casos_por_obitos(df_estado, casos_por_obitos=casos_por_obitos)
+    return figura
 
+def main():
+    df, todos_os_estados = carrega_dataframe(output_path)
+
+    st.title('Análise de Registros de Caso e Óbitos de COVID-19 no Brasil')
     st.markdown('Abaixo um **dataframe**:')
-    st.text_area('area51','aqui uma área de texto (sem formatação?)')
+    # st.text_area('area51','aqui uma área de texto (sem formatação?)')
     # st.dataframe(df_estado.set_index('data').loc['2020-10':'2021-01'])
-    
-    figura = plot_timeseries_casos_por_obitos(df_estado)
+
+    casos_por_obitos =   st.sidebar.selectbox('Selecione o Estado', ['Casos por óbito','Óbitos por caso'], index=0)
+    estado_selecionado = st.sidebar.selectbox('Selecione o Estado', todos_os_estados, index=0)
+
+    figura = carrega_dados_e_plota(df, estado_selecionado, casos_por_obitos=casos_por_obitos)
     st.pyplot(fig=figura)
 
 if __name__ == '__main__':
@@ -29,8 +35,3 @@ if __name__ == '__main__':
     output_path = 'HIST_PAINEL_COVIDBR_12jan2021.zip'
     download_url(url, output_path) 
     main()
-# "Comandos mágicos do streamlit"
-# df_estado
-
-
-
--- src/helpers.py
+++ src/helpers.py
@@ -24,10 +24,10 @@ def load_dataframe(caminho, verbose=True):
 
 def preprocessamento(df):
     df['data'] = pd.to_datetime(df['data'])
-    df['obitos_por_casos_novos']     = np.log10(df['obitosNovos']/df['casosNovos'])
-    df['obitos_por_casos_acumulado'] = np.log10(df['obitosAcumulado']/df['casosAcumulado'])
-    df['casos_por_obitos_novos']     = np.log10(df['casosNovos']/df['obitosNovos'])
-    df['casos_por_obitos_acumulado'] = np.log10(df['casosAcumulado']/df['obitosAcumulado'])
+    df['obitos_por_casos_novos']     = 100*(df['obitosNovos']/df['casosNovos'])
+    df['obitos_por_casos_acumulado'] = 100*(df['obitosAcumulado']/df['casosAcumulado'])
+    df['casos_por_obitos_novos']     = (df['casosNovos']/df['obitosNovos'])
+    df['casos_por_obitos_acumulado'] = (df['casosAcumulado']/df['obitosAcumulado'])
     return df
 
 def casos_por_obitos_do_estado(df, estado='RS', casos_por_obitos = 1):
@@ -38,21 +38,31 @@ def casos_por_obitos_do_estado(df, estado='RS', casos_por_obitos = 1):
         
     df_estado = df_estado[df_estado['codmun'].isna()]
     
-    if casos_por_obitos==1:
+    if casos_por_obitos=='Casos por óbito':
         value_vars = ['casos_por_obitos_novos', 'casos_por_obitos_acumulado']
     else:
         value_vars = ['obitos_por_casos_novos', 'obitos_por_casos_acumulado']
     df_estado = df_estado.melt(id_vars='data', value_vars=value_vars).rename_axis(estado, axis='columns') # 'casosAcumulado' 'obitosAcumulado'
     return df_estado
 
-def plot_timeseries_casos_por_obitos(df_estado):
+def plot_timeseries_casos_por_obitos(df_estado, casos_por_obitos=1):
     fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(11,7))
-    sns_fig = sns.lineplot(data=df_estado, x='data', y='value', hue='variable', ax=ax)
+    sns.lineplot(data=df_estado, x='data', y='value', hue='variable', ax=ax)
+      
+    if casos_por_obitos=='Casos por óbito':
+        ax.set_ylabel('Casos por Óbito')
+        ax.set_yscale('log')
+        # ax.set_yticks(ticks=np.logspace(0,4))
+        # ax.set_yticklabels(labels=np.logspace(0,4))
+    else: 
+        ax.set_ylabel('Óbitos a cada 100 casos')
+        # ax.set_yticks(ticks=range(5))
+        # ax.set_yticklabels(labels=10**np.array(range(5)))
+        ax.set_ylim([0,10])
 
-    ax.set_ylabel('Casos por Óbito')
     ax.legend(['Novos','Acumulados'])
     ax.set_title(f'Série para o {df_estado.axes[1].name}')
-    ax.set_yticks(ticks=range(5))
-    ax.set_yticklabels(labels=10**np.array(range(5)))
+    
+
 
     return fig
