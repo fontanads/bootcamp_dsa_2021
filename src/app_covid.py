4783d119f523587339b6b94f1feb8314f5495d8c
Bruno Fontana <fontanads@gmail.com>
2021-01-16 15:22:02 -0300

    test

--- src/app_covid.py
+++ src/app_covid.py
@@ -3,23 +3,23 @@ import pandas as pd
 import numpy as np
 # import matplotlib.pyplot as plt 
 
-from helpers import preprocessamento, download_url, load_dataframe, casos_por_obitos_do_estado, plot_timeseries_casos_por_obitos
+# from helpers import preprocessamento, download_url, load_dataframe, casos_por_obitos_do_estado, plot_timeseries_casos_por_obitos
 
-@st.cache
-def carrega_dataframe(output_path):
-    df = load_dataframe(output_path)
-    df = preprocessamento(df)
-    todos_os_estados   = sorted(df['estado'].dropna().unique().tolist())
-    todos_os_estados.insert(0,'Brasil')
-    return df, todos_os_estados
+# @st.cache
+# def carrega_dataframe(output_path):
+#     df = load_dataframe(output_path)
+#     df = preprocessamento(df)
+#     todos_os_estados   = sorted(df['estado'].dropna().unique().tolist())
+#     todos_os_estados.insert(0,'Brasil')
+#     return df, todos_os_estados
 
-def carrega_dados_e_plota(df, estado_selecionado, casos_por_obitos='Casos por óbito'):
-    df_estado = casos_por_obitos_do_estado(df, estado=estado_selecionado, casos_por_obitos=casos_por_obitos)
-    figura = plot_timeseries_casos_por_obitos(df_estado, casos_por_obitos=casos_por_obitos)
-    return figura
+# def carrega_dados_e_plota(df, estado_selecionado, casos_por_obitos='Casos por óbito'):
+#     df_estado = casos_por_obitos_do_estado(df, estado=estado_selecionado, casos_por_obitos=casos_por_obitos)
+#     figura = plot_timeseries_casos_por_obitos(df_estado, casos_por_obitos=casos_por_obitos)
+#     return figura
 
 def main():
-    df, todos_os_estados = carrega_dataframe(output_path)
+    # df, todos_os_estados = carrega_dataframe(output_path)
 
     st.title('Análise de Registros de Caso e Óbitos de COVID-19 no Brasil')
     # st.markdown('Abaixo um **dataframe**:')
--- src/helpers.py
+++ src/helpers.py
@@ -1,5 +1,5 @@
 import pandas as pd 
-# import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt 
 import seaborn as sns
 import numpy as np
 
